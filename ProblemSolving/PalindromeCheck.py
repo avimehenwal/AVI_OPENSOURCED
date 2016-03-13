d87a53b4572e06df314b32c779e4e373eff6a1ea
avimehenwal <avi.mehanwal@gmail.com>
2016-03-13 15:40:25 +0530

    is_palindrome method

--- ProblemSolving/PalindromeCheck.py
+++ ProblemSolving/PalindromeCheck.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python
 
-__author__ = 'avimehenwal'
-__date__ = '26-March-2015'
+__author__  = 'avimehenwal'
+__date__    = '13-March-2016'
 __purpose__ = 'Palindrome Check'
 
 
 """
 Check if a palindrome could be genearted from a given string or not
 """
+
+def is_palindrome(string):
+    reverse_string = string[::-1]
+    if string == reverse_string:
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    strings = ['avi', 'avva', 'f', 'ds', 'gg', 'misssisssim', 'missisippy']
+    for string in strings:
+        if is_palindrome(string):
+            print string, 'is PALINDROME'
+        else:
+            print string, 'is NOT palindrome'
 
